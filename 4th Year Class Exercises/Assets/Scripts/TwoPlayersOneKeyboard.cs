025c6d0a52ab3b2b8a5f24d31682c8fba163ed1f
Lu-Wannell <2727686@students.wits.ac.za>
2026-02-19 15:25:25 +0200

    Added Jumping!

--- 4th Year Class Exercises/Assets/Scripts/TwoPlayersOneKeyboard.cs
+++ 4th Year Class Exercises/Assets/Scripts/TwoPlayersOneKeyboard.cs
@@ -1,3 +1,5 @@
+using System.Runtime.CompilerServices;
+using Unity.VisualScripting;
 using UnityEngine;
 using UnityEngine.InputSystem;
 
@@ -5,24 +7,39 @@ public class TwoPlayersOneKeyboard : MonoBehaviour
 {
     [Header("Actions (drag from your Input Actions asset")] // Adds a label in the inspector for organisation
     [SerializeField] private InputActionReference p1Move;
+    [SerializeField] private InputActionReference p1Jump;
+
     [SerializeField] private InputActionReference p2Move;
+    [SerializeField] private InputActionReference p2Jump;
 
     [Header("Players")]
     [SerializeField] private Transform p1;       //Transform for Player One
     [SerializeField] private Transform p2;       //Transform for Player Two
 
+    [SerializeField] private Rigidbody rb1;
+    [SerializeField] private Rigidbody rb2;
+
     [SerializeField] private float speed = 5f;
+    [SerializeField] private float jumpVelocity = 5f;
+
+    [Header("Level")]
+    [SerializeField] private bool p1IsGrounded = true;
+    [SerializeField] private bool p2IsGrounded = true;
 
     private void OnEnable()        //Called when the component becomes enabled/active
     {
         p1Move.action.Enable();          //Enables player One's input action so it can read input
+        p1Jump.action.Enable();
         p2Move.action.Enable();          //Enables player Two's input action so it can read input
+        p2Jump.action.Enable();
     }
 
     private void OnDisable()        //Called when the component becomes disabled/inactive
     {
         p1Move.action.Disable();        //Disables player One's action (stops reading input)
+        p1Jump.action.Disable();
         p2Move.action.Disable();        //Disables player Two's action (stops reading input)
+        p2Jump.action.Disable();
     }
 
     private void Update()
@@ -32,6 +49,43 @@ public class TwoPlayersOneKeyboard : MonoBehaviour
 
         if (p1) p1.position += new Vector3(m1.x, 0f, m1.y) * speed * Time.deltaTime;       //Only move player 1 if the transform reference exists;
         if (p2) p2.position += new Vector3(m2.x, 0f, m2.y) * speed * Time.deltaTime;       //Only move player 2 if the transform reference exists;
+
+        if (p1Jump.action.ReadValue<float>() == 1 && p1IsGrounded)
+        {
+            rb1.AddForce(Vector3.up * jumpVelocity, ForceMode.Impulse);
+            p1IsGrounded = false;
+        }
+
+
+        if (p2Jump.action.ReadValue<float>() == 1 && p2IsGrounded)
+        {
+            rb2.AddForce(Vector3.up * jumpVelocity, ForceMode.Impulse);
+            p2IsGrounded = false;
+        }
+
+        Collider[] hitcolliders = Physics.OverlapSphere(rb1.position, 1f);
+        foreach (var hitcollider in hitcolliders)
+        {
+            if (hitcollider.tag == "Ground")
+            {
+                p1IsGrounded = true;
+            }
+        }
+
+        Collider[] hitcolliders2 = Physics.OverlapSphere(rb2.position, 1f);
+        foreach (var hitcollider in hitcolliders2)
+        {
+            if (hitcollider.tag == "Ground")
+            {
+                p2IsGrounded = true;
+            }
+        }
+
+
+    }
+    private void OnDrawGizmos()
+    {
+        Gizmos.DrawSphere(rb1.position, 1f);
     }
 
 }
